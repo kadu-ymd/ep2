2b70d948d970d1ee3464f45e08f4ee44dad2617b
Paola <paolabbsf2910@gmail.com>
2021-05-09 19:54:56 -0300

    commit Paola 8

--- teste.py
+++ teste.py
@@ -1,4 +1,5 @@
 import funcs
+# import colorama
 
 # def cores_naipe(cartas):
 cartas = funcs.rand_lista(funcs.cria_baralho())
@@ -25,3 +26,11 @@ print(cartas_cores)
 # instalar
 anaconda = 'conda install -c anaconda colorama'
 vscode = 'pip install colorama'
+
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
